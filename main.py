e86cb1c72ba25571c5b38403b0a203da47d44b58
Brian Hines <brian@projectweekend.net>
2015-11-07 17:46:53 -0600

    That's it

---
+++ main.py
@@ -0,0 +1,19 @@
+import serial
+from picloud_client import PiCloud
+
+
+SERIAL_PORT = '/dev/ttyAMA0'
+SERIAL_RATE = 9600
+PICLOUD_EVENT = 'home:thpl'
+
+
+def main():
+    picloud = PiCloud()
+    ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
+    while True:
+        sensor_json = ser.readline()
+        picloud.publush(event=PICLOUD_EVENT, data=sensor_json)
+
+
+if __name__ == "__main__":
+    main()
