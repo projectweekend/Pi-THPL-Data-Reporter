a517402fe13514108226cc1210d4396739adaf2f
Brian Hines <brian@projectweekend.net>
2015-11-28 15:45:32 -0600

    Let's add some logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import serial
 from picloud_client import PiCloud
 
@@ -7,12 +8,23 @@ SERIAL_RATE = 9600
 PICLOUD_EVENT = 'home:thpl'
 
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    filename='/tmp/thpl-data-reporter.log',
+    filemode='w')
+
+
 def main():
     picloud = PiCloud(client_name='THPL-Data-Reporter')
     ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
     while True:
-        sensor_json = ser.readline()
-        picloud.publish(event=PICLOUD_EVENT, data=sensor_json)
+        try:
+            sensor_json = ser.readline()
+            picloud.publish(event=PICLOUD_EVENT, data=sensor_json)
+        except Exception as e:
+            logging.exception(e)
+            raise e
 
 
 if __name__ == "__main__":
